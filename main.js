dd0a60bf699a0e738236a38edb34b1afcf85a977
Dev-PeterWachira <wmainap@gmail.com>
2024-10-17 16:16:34 +0300

    simple liker app

--- main.js
+++ main.js
@@ -2,8 +2,49 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
 
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall() 
+      .then(() => {
+        if (heart.textContent === "♡") {
+          heart.textContent = "♥";
+          heart.classList.add("activated-heart");
+                  } else {
+          heart.textContent = "♡";
+          heart.classList.remove("activated-heart"); 
+        }
+      })
+      .catch((error) => {
+        errorModal.classList.remove("hidden");
+        const errorMessage = document.getElementById("modal-message");
+        errorMessage.textContent = error; 
+
+        setTimeout(() => {
+          errorModal.classList.add("hidden"); 
+        }, 3000);
+      });
+    });
+  });
+});
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
 
 
 
